9509e437145fa97ddb184de2cb6146ccea48e4e4
Jarek <Jarek@Jarek-Komputer>
2017-04-20 21:53:45 +0200

    Third change in Anagram for GitOne

--- src/gittwotest/MainGit2.java
+++ src/gittwotest/MainGit2.java
@@ -12,5 +12,6 @@ package gittwotest;
 public class MainGit2 {
     public static void main(String[] args) {
         System.out.println("First change");
+        System.out.println("Second change");
     }
 }
